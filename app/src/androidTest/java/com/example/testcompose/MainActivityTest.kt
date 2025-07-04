8aefbe1838f6abdb63c3ec3eaf2b0be4d03d3c3c
Eric <NT90947@cathaybk.com.tw>
2025-07-04 12:11:31 +0800

    新增測試log

--- app/src/androidTest/java/com/example/testcompose/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/testcompose/MainActivityTest.kt
@@ -15,6 +15,7 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.hamcrest.Matchers.`is`
+import android.util.Log
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
@@ -27,24 +28,28 @@ class MainActivityTest {
 
     @Test
     fun customTextFieldView_isDisplayed() {
+        Log.d("TestLog", "開始測試 customTextFieldView 是否顯示")
         composeTestRule
             .onNodeWithTag("searchTextField")
             .assertIsDisplayed()
+        Log.d("TestLog", "customTextFieldView 已顯示")
     }
 
     @Test
     fun firstFavoriteCollection_showsKnightText() {
-        // 檢查 LazyFavoriteCollectionByFavoriteData 第一個 item 是否為 "Knight"
+        Log.d("TestLog", "開始測試第一個 favoriteCollection 是否顯示 Knight")
         composeTestRule
             .onAllNodesWithText("Knight")[1] // 第二個 "Knight"，通常第一個在 alignYourBodyData，第二個在 favoriteCollectionData
             .assertIsDisplayed()
+        Log.d("TestLog", "Knight 已顯示於第一個 favoriteCollection")
     }
 
     @Test
     fun firstFavoriteCollection_showsReynoidText() {
-        // 檢查 favoriteCollectionData 的 "Reynoid"（第二個出現的 "Reynoid"）
+        Log.d("TestLog", "開始測試 favoriteCollection 是否顯示 Reynold")
         composeTestRule
             .onAllNodesWithText("Reynoid")[1]
             .assertIsDisplayed()
+        Log.d("TestLog", "Reynoid 已顯示於 favoriteCollection")
     }
 }
